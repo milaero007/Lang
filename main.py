f644343f8cfdd35723aee288427186db18a619a0
milaero007 <34268125+milaero007@users.noreply.github.com>
2023-02-19 18:00:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import LLPlay
 import LLStorage
 import LLFiller
+from tkinter import messagebox
 
 #DB initialisation
 sqlite = LLStorage.SQLite()
-LLPlay.run_play(sqlite)
-#LLFiller.run_filler(sqlite)
+see_more = messagebox.askyesno(title='What to do',  message='Would you like to run test?', detail='Click NO to start filling')
+if not see_more:
+    LLFiller.run_filler(sqlite)
+    exit(0)
+else:
+    LLPlay.run_play(sqlite)
+    exit(0)
+    
